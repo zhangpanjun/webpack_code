8bf2eecb9abc39cee6a476f1d8f5f5a344b0c8e8
zhangpanjun <31738835+zhangpanjun@users.noreply.github.com>
2024-09-11 14:26:01 +0800

    Add files via upload

---
+++ webpack.config.js
@@ -0,0 +1,39 @@
+
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+module.exports = {
+  // 入口
+  // 相对路径和绝对路径都行
+  entry: "./src/main.js",
+  // 输出
+  output: {
+    // path: 文件输出目录，必须是绝对路径
+    // path.resolve()方法返回一个绝对路径
+    // __dirname 当前文件的文件夹绝对路径
+    path: path.resolve(__dirname, "dist"),
+    // filename: 输出文件名
+    filename: "static/js/main.js", // 将 js 文件输出到 static/js 目录中
+    clean: true, // 自动将上次打包目录资源清空
+  },
+  // 加载器
+  module: {
+    rules: [
+        {
+        // 用来匹配 .css 结尾的文件
+        test: /\.css$/,
+        // use 数组里面 Loader 执行顺序是从右到左
+        use: ["style-loader", "css-loader"],
+        },
+    ],
+  },
+  // 插件
+  plugins: [
+    new HtmlWebpackPlugin({
+        // 以 public/index.html 为模板创建文件
+        // 新的html文件有两个特点：1. 内容和源文件一致 2. 自动引入打包生成的js等资源
+        template: path.resolve(__dirname,'public/index.html')
+    })
+  ],
+  // 模式
+  mode: "development", // 开发模式
+};
